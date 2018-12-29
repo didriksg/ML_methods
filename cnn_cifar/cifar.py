8b430e67e81c507277b49dac4b34c7cb30cfc2fd
Unknown <kiddid96@gmail.com>
2018-12-29 15:04:20 +0100

    Add data plotting and empty functions

--- cnn_cifar/cifar.py
+++ cnn_cifar/cifar.py
@@ -1,4 +1,5 @@
 import random
+from typing import Set
 
 import matplotlib.pyplot as plt
 import numpy as np
@@ -8,13 +9,43 @@ from tflearn.layers.conv import conv_2d, max_pool_2d
 from tflearn.layers.core import input_data, dropout, fully_connected
 from tflearn.layers.estimator import regression
 
-
 # Hyperparams
 LEARNING_RATE = 0.001
 EPOCHS = 10
 BATCH_SIZE = 32
 
 MODEL_NAME = 'cifar_model'
+
+TRAINING = False
+
+CIFAR_LABELS = {'Airplane', 'Automobile', 'Bird', 'Cat', 'Deer', 'Dog', 'Frog', 'Horse', 'Ship', 'Truck'}
+
+
+def cifar_model():
+    pass
+
+
+def augmentation():
+    pass
+
+
+def plt_random():
+    pass
+
+
+def plt_wrong():
+    pass
+
+
+def get_data_information(data, labels, plot=False):
+    print("Number of Images:", len(data))
+    print("Number of Labels::", len(labels))
+
+    if plot:
+        fig, ax = plt.subplot()
+        plt.bar(np.arrange(1, 10), [len(i) for i in data])
+        ax.set_xlabels(CIFAR_LABELS)
+        plt.show()
 
 
 
@@ -27,12 +58,7 @@ def main():
     train_img, train_labels = train
     validation_img, validation_labels = validation
 
-    plt.figure()
-    test_image = train_img[100]
-
-    plt.figure()
-    plt.imshow(test_image)
-    plt.show()
+    get_data_information(train_img, train_labels, plot=True)
 
 
 if __name__ == '__main__':
