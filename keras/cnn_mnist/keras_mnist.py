2e4c56dbb2f87900fe382c9e4c7622591fc7ca09
Unknown <kiddid96@gmail.com>
2019-01-02 22:59:24 +0100

    Correctly understand data

--- keras/cnn_mnist/keras_mnist.py
+++ keras/cnn_mnist/keras_mnist.py
@@ -19,7 +19,7 @@ def show_random_wrong_prediction(model, val_data, val_labels):
 
 def show_image_and_label(img, label):
     plt.figure()
-    plt.title(np.argmax(label))
+    plt.title(label)
     plt.xticks([])
     plt.yticks([])
     plt.imshow(img, cmap='Greys')
@@ -28,10 +28,15 @@ def show_image_and_label(img, label):
 
 def main():
     # Load data
+    # Data are array of np arrays with pixel intensity from 0-255 in one channel, as the data is greyscaled
+    # Labels is an array containing the label as an int on the associated data index
     (train_data, train_labels), (val_data, val_labels) = mnist.load_data()
 
-    # Show an image to verify that we loaded the data correctly
+    # Show an image to verify that the data is loaded and understood correctly
     show_image_and_label(train_data[0], train_labels[0])
+    show_image_and_label(val_data[0], val_labels[0])
+
+
 
 
 if __name__ == '__main__':
