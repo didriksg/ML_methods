26d854c806f556fc1dc4ecb4dbf09584c4240765
Unknown <kiddid96@gmail.com>
2019-01-08 00:29:14 +0100

    Add comments to constants

--- keras_implementations/constants.py
+++ keras_implementations/constants.py
@@ -1,5 +1,9 @@
+# Folder used to save the weights of a trained model
 WEIGHTS_BASE_DIR = "weights/"
+# The extension used for saving the models. Should always be '.h5'
 WEIGHTS_EXTENSION = ".h5"
+# Add a timestamp to the weight filename
 NAME_WITH_TIME = False
 
+# Validation split value. Used if no validation data is specified
 VALIDATION_SPLIT = 0.1
