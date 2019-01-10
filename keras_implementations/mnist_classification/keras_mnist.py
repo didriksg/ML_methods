46339b61011d6bd2e9feff343a4730cce4f39050
Unknown <kiddid96@gmail.com>
2019-01-10 21:50:38 +0100

    Rename folders

--- keras_implementations/mnist_classification/keras_mnist.py
+++ keras_implementations/mnist_classification/keras_mnist.py
@@ -1,4 +1,6 @@
 from keras.datasets import mnist
+from keras.datasets import fashion_mnist
+from keras.preprocessing.image import ImageDataGenerator
 import os, sys
 
 sys.path.insert(1, os.path.join(sys.path[0], '../../'))
@@ -13,8 +15,8 @@ from utils import evaluate_model, predict, name_model
 
 # Hyperparams
 LEARNING_RATE = 0.0005
-EPOCHS = 5
-BATCH_SIZE = 1000
+EPOCHS = 10
+BATCH_SIZE = 64
 IMAGE_WIDTH = IMAGE_HEIGHT = 28
 CHANNELS = 1
 
@@ -23,7 +25,7 @@ TRAINING = False
 AUGMENT = True
 USE_IMPROVED = True
 SHOW_PREDICTIONS = True
-ONLY_WRONGS = True
+ONLY_WRONGS = False
 
 # Augmentation settings
 ROTATION = 12
@@ -36,7 +38,7 @@ VERBOSE = 1
 
 # Constants
 MODELS_BASE_DIR = "weights/"
-MODEL_NAME = 'mnist_model_cnn' + ('_improved' if USE_IMPROVED else '')
+MODEL_NAME = 'fashion_mnist' + ('_improved' if USE_IMPROVED else '')
 
 
 def main():
@@ -44,7 +46,7 @@ def main():
     # Load data
     # Data are array of np arrays with pixel intensity from 0-255 in one channel, as the data is greyscale
     # Labels is an array containing the label as an int on the associated data index
-    (x_train, y_train), (x_test, y_test) = mnist.load_data()
+    (x_train, y_train), (x_test, y_test) = fashion_mnist.load_data()
 
     # Preprocess data to fit their network
     x_train = x_train.reshape([-1, IMAGE_WIDTH, IMAGE_HEIGHT, CHANNELS])
@@ -52,20 +54,20 @@ def main():
 
     # Import the preferred from the models defined in 'models.py'
     if USE_IMPROVED:
-        model = improved_cnn(lr=LEARNING_RATE, shape=x_train.shape[1:])
+        model = improved_cnn(lr=LEARNING_RATE, shape=x_train.shape[1:], num_classes=10)
     else:
         model = cnn_model(lr=LEARNING_RATE, shape=x_train.shape[1:])
 
-    augment = None if not AUGMENT else get_augmentation(rotation=ROTATION,
-                                                        width_sr=WIDTH_SHIFT,
-                                                        height_sr=HEIGHT_SHIFT,
-                                                        shear=SHEAR_RANGE,
-                                                        zoom=ZOOM_RANGE
-                                                        )
+    augment = None if not AUGMENT else ImageDataGenerator(rotation_range=ROTATION,
+                                                          width_shift_range=WIDTH_SHIFT,
+                                                          height_shift_range=HEIGHT_SHIFT,
+                                                          shear_range=SHEAR_RANGE,
+                                                          zoom_range=ZOOM_RANGE)
+
 
     # If we are training the model, then train the model
     if TRAINING:
-        train_model(model, x_train, y_train, (x_test, y_test),
+        train_model(model, x_train, y_train, x_test, y_test,
                     batch_size=BATCH_SIZE,
                     epochs=EPOCHS,
                     augment=augment,
